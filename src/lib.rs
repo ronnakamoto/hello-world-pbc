4628feec580adc9813b940809ee3e4ceed88acde
ronnakamoto <adarshron@Adarshs-MacBook-Air.local>
2022-12-31 10:07:00 +0530

    feat: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+//! hello-world
+
+#[macro_use]
+extern crate pbc_contract_codegen;
+
+use pbc_contract_common::context::{ContractContext};
+
+
+/// This is the state of the contract which is persisted on chain.
+///
+/// The #\[state\] macro generates serialization logic for the struct.
+///
+/// ### Fields:
+///
+///   * `name`: [`String`], the hello world greeting.
+///
+///
+#[state]
+struct ContractState {
+    name: String,
+}
+
+impl ContractState {
+    fn greet(&mut self, name: &str) {
+        self.name = "Hello ".to_string() + name;
+    }
+}
+
+#[init]
+fn initialize(
+    _ctx: ContractContext
+) -> ContractState {
+
+    let state = ContractState {
+        name: "Hello World".to_string(),
+    };
+
+    state
+}
+
+#[action(shortname = 0x01)]
+fn greet(
+    _context: ContractContext,
+    state: ContractState,
+    name: String,
+) -> ContractState {
+    assert!(!name.is_empty(), "name must not be empty");
+
+    let mut new_state = state;
+    new_state.greet(&name);
+
+    new_state
+}
+
